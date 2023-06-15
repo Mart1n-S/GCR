3e67fce21bd130b375300b05d7157297ee0f07ad
Martin <105633308+Mart1n-S@users.noreply.github.com>
2023-06-15 11:52:41 +0200

    Delete include directory

--- include/configurationBDD.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bddDNS = 'mysql:host=localhost;dbname=gsb1_1';
-$bddUser = 'root';
-$bddMotDePasse = '';
-$options = [
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false
-];
